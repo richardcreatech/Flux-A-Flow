b7928e75eefca7d363ced5f82a512a71121eb41c
richardcreatech <richardcreatech@gmail.com>
2026-05-24 18:47:49 +0100

    Added an idle component for the marketplace

--- frontend/src/pages/app/Marketplace.jsx
+++ frontend/src/pages/app/Marketplace.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../../styles/market.css";
 import Aside from "../../components/Aside";
 import { useNavigate } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function MarketPlace() {
   const [input, setInput] = useState("");
@@ -132,7 +135,11 @@ function MarketPlace() {
       <Aside />
 
       <section id="main-page">
-     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt quod atque ea maxime, dolorem iure reprehenderit ullam porro vero ab deleniti soluta. Saepe, optio quia. Illum error eligendi eius blanditiis, tempora nesciunt est nostrum ratione necessitatibus! Pariatur, fugit? Dolorum cum, asperiores dolore quod cumque ut. Ea minima autem facere sint voluptas esse nesciunt tenetur ipsum amet, adipisci numquam iste odio distinctio in incidunt officia aliquam earum eius voluptate maxime aspernatur sunt et! Nulla, et suscipit? Asperiores rem sapiente laborum similique corrupti in, sint doloremque temporibus voluptatibus officiis, recusandae libero debitis assumenda obcaecati. Rem quas consectetur cumque excepturi voluptates id quo magni voluptas a asperiores vel qui laboriosam debitis dolorum magnam, quos, eum quia ipsum sint aperiam ea voluptatum. Ullam nemo maxime tempora blanditiis architecto sunt aspernatur, error vitae! Aut deserunt veritatis consectetur perferendis. Adipisci maiores dolor tempora, repellat cum reiciendis explicabo ab totam ad incidunt ratione expedita dolorem necessitatibus atque delectus vero libero sunt voluptatem dolorum consectetur officiis blanditiis itaque iusto! Nesciunt harum non iste ipsum veritatis nisi cum culpa deserunt eos impedit, aperiam quod sunt in. Eveniet, id. Necessitatibus, enim iste? Dignissimos odio dolore blanditiis facilis, enim unde. Molestiae quae ipsum voluptate similique odio possimus debitis corporis, ipsam animi. Dolorem reprehenderit nostrum exercitationem corrupti eaque soluta quisquam, odio quam blanditiis sit similique a cumque vero fugit consectetur aliquam iste! Officiis deserunt quo facilis vel doloribus ipsam non aut recusandae omnis tempore tempora voluptas dolores, totam tenetur sunt cum quos commodi! Et, dolorum cumque! Provident excepturi id reiciendis, natus quas quia dolore deleniti mollitia earum incidunt architecto at aperiam fugiat dolorum eaque unde ex nam quasi asperiores odit suscipit? Quasi ratione assumenda molestias a odit incidunt officia, totam est possimus sapiente distinctio tenetur ea aspernatur eius aliquid quis modi cupiditate sint harum eum eaque ducimus rem? Ab quas soluta expedita ducimus debitis veniam ea non quos iusto excepturi porro perferendis quod, repellendus animi in fuga, beatae, placeat sed hic? Possimus modi qui ex neque nostrum veritatis et natus a facilis voluptatem laborum ad reprehenderit est sequi velit ut, tempore dolorum commodi reiciendis sit tenetur odio repellat similique. Possimus, vitae commodi optio sapiente voluptatem ratione, molestiae adipisci magnam asperiores quia rerum eius explicabo tenetur error ab reiciendis est iure ducimus ex excepturi culpa. Laudantium velit a deserunt minima, inventore officia expedita nulla debitis ea nihil, exercitationem autem, illo aspernatur commodi ratione? Rerum, quasi consequatur. Excepturi laborum quis consectetur veniam dolor necessitatibus.
+        <main id="marketplace_idle">
+          <img src={logo} alt="" />
+          <p>There are No Marketplaces</p>
+          <br /> <button id="create_a_marketplace">< FontAwesomeIcon icon={faPlus} />Create a Marketplace</button>
+        </main>
       </section>
     </main>
   );
