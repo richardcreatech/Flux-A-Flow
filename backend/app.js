2b549f2ac18f4fcf4bc2b4ce0ab4ba021dbad66d
richardcreatech <richardcreatech@gmail.com>
2026-05-25 10:37:41 +0100

    Trying to merge changes from Salems Branch

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors(
-  {origin: "http://localhost:5173",
+  {origin: ["http://localhost:5173", "http://localhost:5174"],
    methods: ["GET", "POST", "PUT", "DELETE"],
    credentials: true}
 )); 
--- backend/model/db.js
+++ backend/model/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/flux-a-flow";
+const MONGO_URI = "mongodb://127.0.0.1:27017/flux-a-flow-test";
 
 mongoose.connect(MONGO_URI).then(() => {
   console.log("Connected to database successfully");
