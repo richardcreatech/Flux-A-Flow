9e12bc80ce8c2ffd2478431463780e85ba6f80af
richardcreatech <richardcreatech@gmail.com>
2026-06-02 14:54:29 +0100

    Fixed some core issues

--- backend/app.js
+++ backend/app.js
@@ -12,20 +12,16 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["http://localhost:5173", "http://localhost:5174"],
-//     methods: ["GET", "POST"],
-//   },
-// });
-
 const io = new Server(server, {
-  cors: {}
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
